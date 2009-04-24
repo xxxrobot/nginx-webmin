80c940d57c82f482f47c913116a6b9f54bfda97b
Robert Rouse <scyllinice@scyllinice.com>
2009-04-24 12:36:07 -0500

    Virtualmin side now sets up a website for a new domain. It will also 'configure' nginx's config directory to contain the virtual hosts based on the vhost path set in the Nginx module settings.

--- virtual_feature.pl
+++ virtual_feature.pl
@@ -1,5 +1,7 @@
 do 'virtualmin-nginx-lib.pl';
 
+use File::Copy;
+
 sub feature_always_links
 {
   
@@ -17,7 +19,66 @@ sub feature_bandwidth
 
 sub feature_check
 {
-  return undef;
+  
+  if($config{'root'} eq "")
+  {
+    return "Nginx module needs a root path to your Nginx installation";
+  }
+  
+  unless(-r $config{'root'} . "/sbin/nginx")
+  {
+    return "Nginx needs to be installed.";
+  }
+  
+  if($config{'vhost_path'} eq "")
+  {
+    return "Nginx module needs a path to your vhost config directory. This directory is usually under the nginx conf path and included in the main nginx conf file.";
+  }
+  
+  unless (-d $config{'root'} . "/conf/" . $config{'vhost_path'})
+  {
+    mkdir($config{'root'} . "/conf/" . $config{'vhost_path'});
+  }
+  
+  unless (-d $config{'root'} . "/conf/" . $config{'vhost_path'} . "/enabled")
+  {
+    mkdir($config{'root'} . "/conf/" . $config{'vhost_path'}. "/enabled");
+  }
+  
+  unless (-d $config{'root'} . "/conf/" . $config{'vhost_path'} . "/disabled")
+  {
+    mkdir($config{'root'} . "/conf/" . $config{'vhost_path'}. "/disabled");
+  }
+  
+  open(CONF, $config{'root'} . "/conf/nginx.conf");
+  
+  local $/ = undef;
+  my $filestring = <CONF>;
+  close(CONF);
+  
+  my $pattern = "include " . $config{'vhost_path'} . "\\/enabled\\/\\*.conf;";
+  
+  #print $pattern;
+  
+  #print $filestring;
+  
+    unless ($filestring =~ /$pattern/) 
+    {
+      
+      chop($filestring);
+      
+      $filestring .= "\tinclude " . $config{'vhost_path'} . "/enabled/*.conf;\n}"; 
+      
+      open(CONF,  ">", $config{'root'} . "/conf/nginx.conf");
+      
+      print(CONF $filestring);
+      
+      close(CONF);
+      
+    }
+    
+    return undef;
+  
 }
 
 sub feature_clash
@@ -28,7 +89,11 @@ sub feature_clash
 sub feature_delete
 {
   my ($d) = @_;
-  &$virtual_server::first_print("Deleting Nginx Site...");
+  &$virtual_server::first_print("Deleting Nginx site ..");
+  
+  unlink($config{'root'} . "/conf/" . $config{'vhost_path'} . "/enabled/" . $d->{'dom'} . ".conf");
+  
+  &$virtual_server::second_print(".. done");
   
 }
 
@@ -39,16 +104,25 @@ sub feature_depends
 
 sub feature_disable
 {
-  
+  my ($d) = @_;
+  &$virtual_server::first_print("Disabling Nginx website ..");
+  move($config{'root'} . "/conf/" . $config{'vhost_path'} . "/enabled/" . $d->{'dom'} . ".conf", $config{'root'} . "/conf/" . $config{'vhost_path'} . "/disabled/" . $d->{'dom'} . ".conf" );
+  &$virtual_server::second_print(".. done");
 }
 
 sub feature_disname
 {
-  
+  return "Nginx website";
 }
 
 sub feature_enable
 {
+  
+  my ($d) = @_;
+  &$virtual_server::first_print("Re-enabling Nginx website ..");
+  move($config{'root'} . "/conf/" . $config{'vhost_path'} . "/disabled/" . $d->{'dom'} . ".conf", $config{'root'} . "/conf/" . $config{'vhost_path'} . "/enabled/" . $d->{'dom'} . ".conf" );
+  &$virtual_server::second_print(".. done");
+  
   
 }
 
@@ -59,7 +133,7 @@ sub feature_import
 
 sub feature_label
 {
-  
+  return "Setup Nginx website for domain?";
 }
 
 sub feature_links
@@ -69,7 +143,7 @@ sub feature_links
 
 sub feature_losing
 {
-  
+  return "The Nginx config file for this website will be deleted.";
 }
 
 sub feature_modify
@@ -81,7 +155,7 @@ sub feature_modify
 
 sub feature_name
 {
-  return "Nginx";
+  return "Nginx website";
 }
 
 sub feature_restore
@@ -92,7 +166,31 @@ sub feature_restore
 sub feature_setup
 {
   my ($d) = @_;
-  &$virtual_server::first_print("Setting up Nginx Site...");
+  &$virtual_server::first_print("Setting up Nginx site ..");
+  
+  my $file;
+  
+  open($file, ">" . $config{'root'} . "/conf/" . $config{'vhost_path'} . "/enabled/" . $d->{'dom'} . ".conf");
+  my $conf = <<CONFIG;
+  server {
+    listen $d->{'ip'}:80;
+    server_name $d->{'dom'} www.$d->{'dom'};
+    
+    location / {
+      
+      root $d->{'home'}/public_html;
+      index index.html index.htm;
+      
+    }
+  }
+CONFIG
+  
+  print($file $conf);
+  
+  close $file;
+  
+  &$virtual_server::second_print(".. done");
+  
 }
 
 sub feature_suitable
